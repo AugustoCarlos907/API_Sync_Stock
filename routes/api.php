c6022ec4e45d6ebe76bd05fb69a57dea8bface6d
Augusto Carlos <augustoaccarlos@gmail.com>
2026-02-01 10:43:04 +0100

    feat: update de routes api

--- routes/api.php
+++ routes/api.php
@@ -4,31 +4,30 @@ use App\Http\Controllers\AlertItemController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\FileController;
+use App\Http\Controllers\StockItemController;
 use Illuminate\Support\Facades\Route;
 
+//login 
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware(['auth:api' , 'throttle:60,1'])->group(function(){
+Route::middleware(['auth:sanctum' , 'throttle:60,1'])->group(function(){
 
-    //login 
-    Route::post('login', [AuthController::class, 'login']);
-    
     //companies
-    Route::get('companies', [CompanyController::class, 'index']);
-    Route::get('companies/{id}', [CompanyController::class, 'show']);
-    Route::post('companies', [CompanyController::class, 'store']);
-
+    Route::get('/companies', [CompanyController::class, 'index']);
+    Route::get('/companies/{id}', [CompanyController::class, 'show']);
+    Route::post('/companies/delete/{id}', [CompanyController::class, 'delete']);
 
     //file upload
-    Route::get('stock/files', [FileController::class, 'showUploadForm']);
-    Route::post('stock/file/upload', [FileController::class, 'uploadFile']);
+    Route::get('/stock/files', [FileController::class, 'index']);
+    Route::post('/stock/file/upload', [FileController::class, 'uploadFile']);
     
+     // stock items
+    Route::get('/stock/items', [StockItemController::class, 'index']);
+
     //stock alerts
-    Route::get('stock/alerts', [AlertItemController::class, 'getAlertItems']);
-    Route::post('stock/alerts', [AlertItemController::class, 'sendStockAlerts']);
+    Route::get('/stock/alerts', [AlertItemController::class, 'getAlertItems']);
+    Route::post('/stock/alerts', [AlertItemController::class, 'sendStockAlerts']);
 
-    // stock items
-    // Listar dados extraídos de um ficheiro
-
-});
+   });
 
 
